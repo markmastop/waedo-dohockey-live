edfa73ca6eb14898a8bd2666f3494313dc3247c6
M. Kram <mark@mastop.net>
2025-06-29 16:30:10 +0200

    Change share button to green color

--- src/components/LiveMatch/ShareButton.tsx
+++ src/components/LiveMatch/ShareButton.tsx
@@ -20,11 +20,11 @@ const ShareButton = ({ matchKey }: ShareButtonProps) => {
         <Button
           onClick={handleShare}
           variant="outline"
-          className="w-full border border-blue-300 text-blue-700 bg-blue-50 hover:bg-blue-100 hover:border-blue-400 py-2 text-base font-semibold"
+          className="w-full border border-green-300 text-green-700 bg-green-50 hover:bg-green-100 hover:border-green-400 py-2 text-base font-semibold"
         >
           <Share2 className="w-4 h-4 mr-2" />
           📱 Share Match Link
-          <Sparkles className="w-4 h-4 ml-2 text-blue-500" />
+          <Sparkles className="w-4 h-4 ml-2 text-green-500" />
         </Button>
       </CardContent>
     </Card>
